c02702dc6a8f25881993e5ae9ee0b5dbeb556f30
Elijah Zupancic <elijah@zupancic.name>
2019-12-19 16:07:00 -0800

    Added serialization id to suppress warnings

--- src/main/java/com/useswiftly/ingestion/records/RecordParseException.java
+++ src/main/java/com/useswiftly/ingestion/records/RecordParseException.java
@@ -4,6 +4,8 @@ package com.useswiftly.ingestion.records;
  *
  */
 public class RecordParseException extends RuntimeException {
+    private static final long serialVersionUID = 4280921967560275961L;
+
     public RecordParseException(final String message) {
         super(message);
     }
